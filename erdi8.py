9857a2736b92986eac123493fb8097157cf91a84
Andreas Thalhammer <athalhammer@users.noreply.github.com>
2021-08-19 23:18:20 +0200

    Update erdi8.py

--- erdi8.py
+++ erdi8.py
@@ -1,6 +1,6 @@
 #    erdi8
 #    Copyright (C) 2021  Andreas Thalhammer
-
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
