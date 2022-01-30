9af8cd0908371aa4fd5f60fc5c21089e0ccd2972
Andreas Thalhammer <andreas@thalhammer.bayern>
2022-01-30 14:01:33 +0100

    added test case for fancy increment

--- test/erdi8_test.py
+++ test/erdi8_test.py
@@ -1,10 +1,10 @@
 import unittest
-import erdi8
+from erdi8 import Erdi8
 
 class E8Test(unittest.TestCase):
 
     def test_inc_enc_dec(self):
-        e8 = erdi8.Erdi8()
+        e8 = Erdi8()
         a = "a"
         for i in range(0, 9999):
             enc = e8.encode_int(i)
@@ -14,15 +14,25 @@ class E8Test(unittest.TestCase):
             a = e8.increment(a)
         self.assertEqual(a, "ifa")
 
+    def test_inc_fancy(self):
+        e8 = Erdi8()
+        current = "erd"
+        target_len = 25 * 33 * 33
+        col_set = set()
+        for i in range(0, target_len):
+            col_set.add(current)
+            current = e8.increment_fancy(current, 12345)
+        self.assertEqual(len(col_set), target_len)
+
     def test_string_check(self):
-        e8 = erdi8.Erdi8()
+        e8 = Erdi8()
         self.assertFalse(e8.check("23"))
         self.assertFalse(e8.check("2ab"))
         self.assertFalse(e8.check("a23l"))
         self.assertFalse(e8.check("ab1"))
 
     def test_safe(self):
-        e8 = erdi8.Erdi8(safe = True)
+        e8 = Erdi8(safe = True)
         self.assertFalse(e8.check("b23a"))
         self.assertFalse(e8.check("b2e3"))
         self.assertFalse(e8.check("bi23"))
